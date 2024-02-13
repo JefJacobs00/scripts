8b1cfb998de9ea0b06ce0c3e85c6ae883c5c3139
Jef Jacobs <jacobsjef16@gmail.com>
2024-02-13 10:41:45 +0100

    updating the installer path based on 32, 64 bit and major version 8 or 7

--- updateNpp.ps1
+++ updateNpp.ps1
@@ -1,15 +1,23 @@
 ﻿$applicationInstalled = $false
 $applicationRegistry = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "Notepad++*" }
 
+Write-Host $applicationRegistry.MajorVersion;
+Write-Host $applicationRegistry.MinorVersion;
+
 if ($applicationRegistry -ne $null) {
     $applicationInstalled = $true
-    Write-Host "Notepad++ is installed."
+    Write-Host "Notepad++ is installed.";
 } else {
-    Write-Host "Notepad++ is not installed."
+    Write-Host "Notepad++ is not installed.";
 }
 
+
 if ($applicationInstalled) {
-    $dlurl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.2/npp.8.6.2.Installer.x64.exe"
+    $64bit = $applicationRegistry.DisplayName.Contains("x64");
+    $v8 = $applicationRegistry.MajorVersion.Equals('8');
+    # v8.6.2/npp.8.6.2.Installer.x64.exe
+    $dlurl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v"+ (&{If($v8){"8.6.2/npp.8.6.2.Installer"} Else {"7.9.5/npp.7.9.5.Installer"}}) + (&{If($64bit){".x64.exe"} Else {".exe"}})
+    Write-Host "Downloading program from $dlurl"
     $installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
     Invoke-WebRequest $dlurl -OutFile $installerPath
     Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
