a74a2138d874f4cc9ed4b61eb76acab6d35d9e5a
JefJacobs00 <43653885+JefJacobs00@users.noreply.github.com>
2024-06-12 10:17:30 +0200

    Add files via upload

---
+++ Enable_lansweeper_WMI.ps1
@@ -0,0 +1,11 @@
+﻿# Run winmgmt -standalonehost
+& winmgmt -standalonehost
+
+# Stop the Windows Management Instrumentation (WMI) service
+Stop-Service Winmgmt -Force
+
+# Start the Windows Management Instrumentation (WMI) service
+Start-Service Winmgmt
+
+# Add a firewall rule for TCP port 24158
+New-NetFirewallRule -DisplayName "WMIFixedPort" -Direction Inbound -LocalPort 24158 -Protocol TCP -Action Allow
